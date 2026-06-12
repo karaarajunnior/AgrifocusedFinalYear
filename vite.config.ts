42ed3e7731bc1370ec8c4fe3c548ef4113a1a65a
honestkwarisiima <honestkwarisiima@gmail.com>
2026-06-12 21:24:57 +0300

    vercel and vite_config_edits

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-	plugins: [react()],
-	optimizeDeps: {
-		exclude: ["lucide-react"],
-	},
+	 plugins: [react()],
+  optimizeDeps: {
+    exclude: ["lucide-react"],
+  },
+  esbuild: {
+    target: "es2020",
+  },
 });
